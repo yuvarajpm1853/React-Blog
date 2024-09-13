6465117b1ee868f84f4a96f5afd52e80f913dfca
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-09-13 23:00:24 +0530

    Added Inline style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,17 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-      <h1>The React Blog</h1>
-      <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
-      </div>
-    </nav>
-     );
-}
- 
-export default Navbar;
+    return (
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>New Blog</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
