4bd9ce6e27bf1706eb6e9f26658c3c86bdd8725d
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-09-14 12:21:00 +0530

    Fn runs when page loaded and when clicked ion button

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,16 @@
 const Home = () => {
-    return (
-      <div className="home">
+  
+  const runOnceDeafult = () => console.log("Welcome");
+  const clickSayHii = () => console.log("Hii");
+  return (
+    <div className="home">
         <h2>Homepage</h2>
+         {/* below fn runs only one time when page gets laoded */}
+         {/* becoz parenthesis with fn name */}
+        <button onClick={runOnceDeafult()}> Click  wishes again</button>
+
+        {/* Runs when user clicks */}
+        <button type="button" onClick={clickSayHii}>Click here to say "Hi"</button>
       </div>
     );
   }
