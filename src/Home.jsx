f0d883bc05c0572dba7fb5a3ad7c1fbe86224484
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-09-17 13:15:38 +0530

    Created custom hook " useFetch" , data : blogs

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,41 +1,9 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from './useFetch';
 
 const Home = () => {
-  
-  const [blogs,setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(false)
-  
-  const deleteBlog = id =>{
-    let newblog = blogs.filter(blog => blog.id != id);
-    setBlogs(newblog);
-  }
-
-  
-  useEffect(()=>{
-    
-   setTimeout(()=>{
-    fetch("http://localhost:8000/blogs")
-   .then(res =>{
-    // console.log(res);
-    if (!res.ok){ // resp obj has key ok , if false 
-      throw Error("Could not able to connect to the Server")
-    }
-    return res.json(); 
-   })
-   .then(data => {setBlogs(data);
-   setIsPending(false);
-  })
-  .catch(err => 
-    {
-      // console.log(err.message);
-      setError(err.message); // catches and set value for var
-      setIsPending(false);
-   })
-},1000) 
-},[]) 
-  
+const {data:blogs,isPending, error} = useFetch("http://localhost:8000/blogs")
+  // grab the data var and call it as blogs in here
   return (
     <div className="home">
       {error && <div> {error}</div>}
---
+++ src/useFetch.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data,setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(false)
+    useEffect(()=>{
+    
+        setTimeout(()=>{
+         fetch(url)
+        .then(res =>{
+            if (!res.ok){ 
+            throw Error("Could not able to connect to the Server")
+            }
+            return res.json(); 
+        })
+        .then(data => {setData(data);
+        setIsPending(false);
+       })
+       .catch(err => 
+         {
+           setError(err.message); 
+           setIsPending(false);
+        })
+     },1000) 
+     },[]) 
+    return {data, isPending, error}
+}
+export default useFetch;
