2d3013827dfac85606260def8e03e1692e0816ed
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-01 16:28:45 +0530

    Added Link for blogs @Home . Updated CSS

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
+
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
 
     return ( 
+      
         <div className="blog-List">
         <h2>{ title }</h2>
          {blogs.map(blog => 
             <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>Wriiten by {blog.author}</p>
+              <Link to={`blogs/${blog.id}`}>
+                <h2>{blog.title}</h2>
+                <p>Wriiten by {blog.author}</p>
+              </Link>
             </div>
           )}
           </div>
