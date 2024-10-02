bd9cd34675acadeff91c6ae76b8894df0dbc957e
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-02 16:27:42 +0530

    Created an form for add new blog. Mapped label to input

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("yuvaraj")
     return ( 
-        <div>Added Blog</div>
+        <div className='create'>
+            <h2>Add Blog</h2>
+            <form action="">
+                <label htmlFor="title">Blog Title :</label>
+                <input type="text" id="title" required value={title} 
+                    onChange={e=>setTitle(e.target.value)}/>
+                <label htmlFor="body">Blog Body :</label>
+                <textarea  id="body" required value={body}
+                    onChange={e=>setBody(e.target.value)}></textarea>
+                <label htmlFor="author">Blog Author : </label>
+                <input type="text" id="author" required value={author}
+                    onChange={e=>setAuthor(e.target.value)}/>
+                <button>Add Blog</button>
+            </form>
+        </div>
      );
 }
  
